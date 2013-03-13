28470b76e9bbf681152395da2a4f8d67a9535957
David Seal <seal@math.msu.edu>
2013-03-13 17:14:24 -0400

    Updated buckley_speeds.
    The secant method now gets an arbitrary function G to play with.

--- apps/buckley_leverett/buckley_speeds.py
+++ apps/buckley_leverett/buckley_speeds.py
@@ -14,95 +14,95 @@ def f (q):
   
 #-----------------------------------------------------------------------------
 def fp(q):
-    """ derivative of flux function. """
+    """ derivative of flux function for Buckley-Leverett. """
 
     return (2.*M*q*(1.-q)) / (q**2 + M*(1.-q)**2)**2
 
-def G(q, fqr=0, qr=0):
-    """ Function used for finding the zero for R-H conditions, which are the
-    solution to the equation:
+#-----------------------------------------------------------------------------
+def secant_method( G, yguess0, yguess1, maxiter=1e5, tol=1e-14):
+    """ Secant method for finding the zeros of the function G(y).
 
-        f'(q) = (f(q) - f(qr)) / (q - qr).
+    The method depends on the two previous values, so the user needs to supply
+    two initial guesses.
 
-    Note that the known values are qr and fqr = f(qr), and we are looking for
-    a solution q to that system.
-
-    This is used in the single case of a Riemann problem
-    that produces a Rarefaction + Shock.
-
-    When G(q) = 0, we get a solution to the above equations.
-    """
-    return ( fp( q ) - ( f(q) - fqr ) / (q-qr) )
-
-def Gp(q, fqr=0, qr=0 ):
-    """ Derivative of G from above.  Used for Newton iteration."""
-
-    raise NotImplemented('you suck and need to implement a derivative!')
-
-def secant_right( qguess0=0.4999, qguess1=0.49999, maxiter=1e5, tol=1e-14 ):
-    """ Secant method for Newton iteration. 
+    Method:
     
-    q = q - G(q) * (q-qnm1) / ( G(q) - G(qnm1) ) 
+        y = y - G(y) * (y-ynm1) / ( G(y) - G(ynm1) ) 
+
+    Parameters:
+    ===========
+
+        G : callable function.
+
+        yguess0, yguess1 : initial guesses for iteration
+
+        maxiter : maximumun number of iterations allowed
+
+        tol : tolerance used to determine if we found the zero.
+
+    Returns:
+
+        y : solution to G(y) = 0.
+
     """
 
-    # initial guesses:
-    qnm1 = qguess0;  Gnm1 = G(qnm1)
-    qn   = qguess1
+    # initial setup:
+    ynm1 = yguess0;  
+    Gnm1 = G(ynm1)
+    yn   = yguess1
 
     n_iters = 0
     while( n_iters < maxiter ):
 
-        Gn   = G(qn)
+        Gn   = G(yn)
         if( abs(Gn) < tol ):
-            return qn
+            return yn
 
         # print('Gn = %f, Gnm1 = %f' % (Gn, Gnm1) )
 
         # update for qn (and swap out the old q):
-        tmp = qn - Gn * (qn-qnm1) / ( Gn - Gnm1 )
+        tmp = yn - Gn * (yn-ynm1) / ( Gn - Gnm1 )
 
-        # update q (and save old variable):
-        qnm1 = qn
+        # update y (and save old variable):
+        ynm1 = yn
         Gnm1 = Gn
-        qn   = tmp
+        yn   = tmp
 
-def secant_left( qguess0=0.13, qguess1=0.130000001, maxiter=1e5, tol=1e-14 ):
-    """ Secant method for Newton iteration. 
-    
-    q = q - G(q) * (q-qnm1) / ( G(q) - G(qnm1) ) 
 
-    For our problem, qs = 0.13397459621556132.
+#-----------------------------------------------------------------------------
+def main():
+    """Single function to compute the two shock speeds that are present for
+    our numerical example.
+   
+    This has not been written in a general fashion - certainly we could
+    rewrite this so it could be used again in the future.
     """
 
-    # initial guesses:
-    qnm1 = qguess0;  Gnm1 = G(qnm1)
-    qn   = qguess1
+    # Riemman state that remains static:
+    q_fixed  = 1
+    f_qfixed = f( q_fixed )
+    def G(qs):
+        """ zero function that needs to be solved for left-state of our
+        example Riemman problem
 
-    qr  = 1
-    fqr = f(qr)
+        R-H conditions:
 
-    n_iters = 0
-    while( n_iters < maxiter ):
+            f'(qs) = (f(qs) - f(q_fixed)) / (q - q_fixed).
 
-#       print('qn = %f' % qn )
-        Gn   = G(qn, fqr, qr)
-        if( abs(Gn) < tol ):
-            return qn
+        """
+        return ( fp( qs ) - ( f(qs) - f_qfixed ) / (qs-q_fixed) )
 
-        # update for qn (and swap out the old q):
-        tmp = qn - Gn * (qn-qnm1) / ( Gn - Gnm1 )
 
-        # update q (and save old variable):
-        qnm1 = qn
-        Gnm1 = Gn
-        qn   = tmp
+    qs_left_problem = secant_method( G, 0.13, 0.130000001 )
 
-def get_shock_speeds():
+    print('left  value qs = %2.15e, speed = %2.15e' % 
+        (qs_left_problem, fp(qs_left_problem)  ) )
 
-    qs_left  = secant_left()
-    qs_right = secant_right()
+    q_fixed  = 0
+    f_qfixed = f(q_fixed)
 
-    print('left  value qs = %2.15e, speed = %2.15e' % (qs_left,  fp(qs_left)  ) )
-    print('right value qs = %2.15e, speed = %2.15e' % (qs_right, fp(qs_right) ) )
+    qs_right_problem = secant_method( G, 0.48, 0.480000001 )
+    print('right value qs = %2.15e, speed = %2.15e' % 
+        (qs_right_problem, fp( qs_right_problem) ) )
 
-    return( qs_left, qs_right )
+    return( qs_left_problem, qs_right_problem )
