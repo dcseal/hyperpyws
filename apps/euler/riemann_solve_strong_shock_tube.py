0511c3eb5b10beabb31c88cf054386e3a66d029f
David Seal <seal@math.msu.edu>
2013-03-19 14:34:30 -0400

    More mods to riemann_solve_strong_shock_tube.
    Cleaner print statements.  Guesses now work for SOD and the `strong shock tube'
    example.

--- apps/euler/riemann_solve_strong_shock_tube.py
+++ apps/euler/riemann_solve_strong_shock_tube.py
@@ -12,10 +12,10 @@ from math import sqrt
 # W_K = [rho, u, press], K=l,r.
 
 # ( TODO: <<<< make these user supplied parameters to a function <<<)
-#W_l = [10.0, 0, 100.0]
-#W_r = [1.0 , 0,   1.0]  
-W_l = [3.0, 0, 3.0]
-W_r = [1.0, 0, 1.0]  
+W_l = [10.0, 0, 100.0]
+W_r = [1.0 , 0,   1.0]  
+#W_l = [3.0, 0, 3.0]
+#W_r = [1.0, 0, 1.0]  
 
 rho_l = W_l[0]; u_l = W_l[1]; p_l = W_l[2]
 rho_r = W_r[0]; u_r = W_r[1]; p_r = W_r[2]
@@ -57,10 +57,7 @@ def f_K( ps, W ):
 
 #===============================================================================
 def fp_K( ps, W ):
-    """ Derivative of the function, f_K. 
-    
-    c.f. eqn (4.37), Toro.  
-    """
+    """ Derivative of the function, f_K."""
 
      # the left (or right) constant variables:
     rho = W[0]
@@ -79,7 +76,10 @@ def fp_K( ps, W ):
         return sqrt( A / ( ps + B ) )*( 1.0 - ( ps-p )/ (2.0*(B+ps) )  )
     else:
         # rarefaction:
-        return c/(gamma*ps) * (ps/p)**(0.5*gm1/gamma)
+        # return c/(gamma*ps) * (ps/p)**(0.5*gm1/gamma)
+
+        # this line is what is in Toro's book (c.f. eqn 4.37):
+        return 1.0 / (rho * c) * (ps/p)**(-0.5*gp1/gamma)
 
 #===============================================================================
 def f( ps ):
@@ -107,56 +107,64 @@ def NewtonSolve( f, fp, yguess, tol=1e-13, maxiter=10000 ):
         fn = f(yn)
         yn = yn - fn / fp( yn )
         n += 1
-#       print( 'yn = %2.7e; fn = %2.15e ' % (yn,fn) )
-
         if( abs(fn) < tol ):
             return yn, n
-
-    print("failed to converge")
+        
+    print("failed to converge;  n = %d" % n)
     raise Exception  # (<<<<< TODO - which exception should we raise? <<<< )
 
 #===============================================================================
-
 # TODO - THIS SECTION NEEDS TO BE MASSIVELY CLEANED UP!
 #
 # Better naming convention, ....  better output data ...
 
-pstar,niters = NewtonSolve( f, fp, 0.5*(p_l+p_r), tol=1e-13 )
+print('Initial data:')
+print('    W_l = (%f, %f, %f) ' % (W_l[0],W_l[1],W_l[2]) )
+print('    W_r = (%f, %f, %f) ' % (W_r[0],W_r[1],W_r[2]) )
+print(' ')
+
+# stupid initial guess, take the arithmetic mean for p:
+pstar,niters = NewtonSolve( f, fp, 0.5*(p_l+p_r), tol=1e-14 )
+
+# compute ustar, which is only a function of pstar:
 ustar        = 0.5*( u_l + u_r ) + 0.5*( f_K( pstar, W_r ) - f_K( pstar, W_l ) )
 
-print('    Newton iteration converged in %d iterations' % niters )
-print('    p_star = %2.15e, u_star = %2.15e' % (pstar,ustar) )
-
-# ustar provides location of contact discontinuity:
-print('  speed of contact discontinuity: %2.15e ' % ustar )
+print('Newton solve converged in %d iterations' % niters )
+print('    p_star = %2.15e' % (pstar) )
+print(' ')
 
 # information about left part:
 if( pstar > p_l ):
-    print(" left shock ")
+    print("Left Shock:")
     shock_speed_left = u_l - c_l*sqrt( (gp1/(2.*gamma))*(pstar/p_l)+gm1/(2*gamma))
-    print("left shock speed = %f" % shock_speed_left )
+    print("     left shock speed = %f" % shock_speed_left )
 else:
-    print(" left rarefaction ")
+    print("Left Rarefaction: ")
 
     rho_star_l = rho_l * ( pstar / p_l )**(1./gamma)
     cs = sqrt( abs( gamma*pstar / rho_star_l ) )
 
     c_rarefaction_left_l = u_l   - c_l
     c_rarefaction_left_r = ustar - cs
-    print("Left foot speed: %2.15e;  Right foot speed: %2.15e" % \
-        (c_rarefaction_left_l, c_rarefaction_left_r ) )
+    print("    Left  foot speed  = %2.15e" % c_rarefaction_left_l )
+    print("    Right foot speed  = %2.15e" % c_rarefaction_left_r )
+
+# ustar provides location of contact discontinuity:
+print('Contact Discontinuity:')
+print('    speed (ustar)     = %2.15e ' % ustar )
 
 # more information about solution on right edge:
 if( pstar > p_r ):
-    print(" right shock ")
+    print("Right Shock:")
     shock_speed_right = u_r + c_r*sqrt( (gp1/(2.*gamma))*(pstar/p_r)+gm1/(2*gamma))
-    print("Right shock speed = %f" % shock_speed_right )
+    print("    Right shock speed = %2.15e" % shock_speed_right )
 else:
-    print(" right rarefaction " )
+    print("Right Rarefaction " )
 
-
+print(' ')
 rho_star_r = \
     rho_r * ( (pstar/p_r) + (gm1/gp1) ) / ( (gm1/gp1)* pstar/ p_r + 1.0 ) 
-print(" rho_star_right = %f " % rho_star_r )
+print("rho_star_right = %f " % rho_star_r )
+print("rho_star_left  = ??? " )
 
 #===============================================================================
