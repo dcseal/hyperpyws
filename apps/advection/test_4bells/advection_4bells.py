21ec2730d06db226a0553fbb33a126070e8646c8
David Seal <seal@math.msu.edu>
2013-10-29 13:54:36 -0400

    Fixed error in usage statement for code.
    Previous versions of the code had a statement about restictions on usage because
    it was copied from other software.  The intention is to use the GPL license.

--- apps/advection/test_4bells/advection_4bells.py
+++ apps/advection/test_4bells/advection_4bells.py
@@ -1,13 +1,6 @@
 #==============================================================================#
 # This file is part of HYPERPYWS: Hyperbolic Python WENO Solver
 #
-#
-#   This software is made available for research and instructional use only.
-#   You may copy and use this software without charge for these non-commercial
-#   purposes, provided that the copyright notice and associated text is
-#   reproduced on all copies.  For all other uses (including distribution of
-#   modified versions), please contact the author at the address given below.
-# 
 #   *** This software is made available "as is" without any assurance that it
 #   *** will work for your purposes.  The software may in fact have defects, so
 #   *** use the software at your own risk.
@@ -95,7 +88,7 @@ def DefineTestCase ():
   test          = TestCase()
   test.ModelEqn = Advection1D(v)
   test.xlims    = [-1.0, 1.0]
-  test.tend     = 8.0
+  test.tend     = 1000.0
   test.BCs      = CreateBC_func
   test.qinit    = q_init
   test.qexact   = q_exact
